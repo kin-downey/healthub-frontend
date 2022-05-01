d2a4502d7030fdc83abd236958a88aeec5c94460
k-downey <kinchan.yamamoto@gmail.com>
2022-05-01 13:57:56 +0900

    [update]アプリケーションバーの作成

---
+++ frontend/src/components/AppBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <v-app-bar color="deep-purple accent-4" dense dark>
+      <v-toolbar-title class="font-weight-bold">Healthub</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </div>
+</template>
