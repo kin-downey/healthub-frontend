36c9f386c7c32c41e4a5710752795f611ca4da0b
k-downey <kinchan.yamamoto@gmail.com>
2022-05-01 16:31:38 +0900

    [update]ナビゲーションドロワーの追加

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <AppBar />
+    <Navigation />
   </div>
 </template>
 
 <script>
 import AppBar from '@/components/AppBar.vue'
+import Navigation from '@/components/Navigation.vue'
 
 export default {
   name: "Home",
   components: {
-    AppBar
+    AppBar,
+    Navigation
   },
 };
 </script>
