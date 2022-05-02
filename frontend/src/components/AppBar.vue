1ffa6480efeb8d76e80479832514a0e653e6661d
k-downey <kinchan.yamamoto@gmail.com>
2022-05-02 20:07:11 +0900

    [update]アプリケーションバーの基本機能完成

--- frontend/src/components/AppBar.vue
+++ frontend/src/components/AppBar.vue
@@ -1,9 +1,45 @@
 <template>
-  <div>
-    <v-app-bar color="deep-purple accent-4" dense dark>
-      <v-toolbar-title class="font-weight-bold">Healthub</v-toolbar-title>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-title class="title"> Healthub </v-list-item-title>
+        <v-btn icon @click="drawer = false">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
+      <v-divider />
+      <v-list nav>
+        <v-list-item v-for="menu in menus" :key="menu.title" :to="menu.url" @click="drawer = false">
+          <v-list-item-icon>
+            <v-icon>{{ menu.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ menu.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-      <v-spacer></v-spacer>
+    <v-app-bar app color="#E64A19">
+      <v-app-bar-nav-icon @click="drawer = true" color="white" />
+      <v-toolbar-title class="font-weight-bold white--text">Healthub</v-toolbar-title>
     </v-app-bar>
-  </div>
+  </v-app>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    drawer: false,
+    menus: [
+      { title: "トップページ", icon: "mdi-web", url: "/" },
+      { title: "ホーム", icon: "mdi-home", url: "/home" },
+      { title: "フレンド", icon: "mdi-account-group", url: "/friends" },
+      { title: "About", icon: "mdi-information-outline", url: "/about" },
+    ],
+  }),
+  methods: {
+  },
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
     <AppBar />
-    <Navigation />
   </div>
 </template>
 
 <script>
 import AppBar from '@/components/AppBar.vue'
-import Navigation from '@/components/Navigation.vue'
 
 export default {
   name: "Home",
   components: {
     AppBar,
-    Navigation
   },
 };
 </script>
