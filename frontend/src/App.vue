c087da789b7d40aaa8649824620a970e9caa08ed
k-downey <kinchan.yamamoto@gmail.com>
2022-05-05 22:10:11 +0900

    [update]認証済みかどうかでナビゲーションを分けた

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,97 @@
 <template>
   <v-app>
-    <v-navigation-drawer app v-model="drawer">
-      <v-list-item>
-        <v-list-item-title class="title"> Healthub </v-list-item-title>
-        <v-btn icon @click="drawer = false">
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-      </v-list-item>
-      <v-divider />
-      <v-list nav>
-        <v-list-item
-          v-for="menu in menus"
-          :key="menu.title"
-          :to="menu.url"
-          @click="drawer = false"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ menu.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ menu.title }}</v-list-item-title>
-          </v-list-item-content>
+    <div v-if="is_login">
+      <v-navigation-drawer app v-model="drawer">
+        <v-list-item>
+          <v-list-item-title class="title"> Healthub </v-list-item-title>
+          <v-btn icon @click="drawer = false">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
         </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+        <v-divider />
+        <v-list nav>
+          <v-list-item
+            v-for="menu in logged_in_menus"
+            :key="menu.title"
+            :to="menu.url"
+            @click="drawer = false"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ menu.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ menu.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        <v-row>
+          <v-col align="center" cols="11">
+            <v-btn
+              @click="drawer = false"
+              to="/signup"
+              color="#EF5350"
+              block
+              class="white--text mx-2 mt-12 my-2"
+              >無料で始めましょう</v-btn
+            >
+            <v-btn
+              @click="drawer = false"
+              to="/login"
+              block
+              outlined
+              class="mx-2"
+              >ログイン</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-navigation-drawer>
+    </div>
+    <div v-else>
+      <v-navigation-drawer app v-model="drawer">
+        <v-list-item>
+          <v-list-item-title class="title"> Healthub </v-list-item-title>
+          <v-btn icon @click="drawer = false">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+        <v-divider />
+        <v-list nav>
+          <v-list-item
+            v-for="menu in public_menus"
+            :key="menu.title"
+            :to="menu.url"
+            @click="drawer = false"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ menu.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ menu.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        <v-row>
+          <v-col align="center" cols="11">
+            <v-btn
+              @click="drawer = false"
+              to="/signup"
+              color="#EF5350"
+              block
+              class="white--text mx-2 mt-12 my-2"
+              >無料で始めましょう</v-btn
+            >
+            <v-btn
+              @click="drawer = false"
+              to="/login"
+              block
+              outlined
+              class="mx-2"
+              >ログイン</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-navigation-drawer>
+    </div>
 
     <v-app-bar app color="#E64A19">
       <v-app-bar-nav-icon @click="drawer = true" color="white" />
@@ -41,15 +109,41 @@
 export default {
   name: "App",
   data: () => ({
+    is_login: false,
     drawer: false,
-    menus: [
+    public_menus: [
+      { title: "トップページ", icon: "mdi-web", url: "/" },
+      {
+        title: "About",
+        icon: "mdi-information-outline",
+        url: "/about",
+      },
+    ],
+    logged_in_menus: [
       { title: "トップページ", icon: "mdi-web", url: "/" },
       { title: "ホーム", icon: "mdi-home", url: "/home" },
       { title: "データ", icon: "mdi-chart-line", url: "/graph" },
-      { title: "フレンド", icon: "mdi-account-group", url: "/friends" },
-      { title: "About", icon: "mdi-information-outline", url: "/about" },
+      {
+        title: "フレンド",
+        icon: "mdi-account-group",
+        url: "/friends",
+      },
+      {
+        title: "About",
+        icon: "mdi-information-outline",
+        url: "/about",
+      },
     ],
   }),
-  methods: {},
+  mounted() {
+    this.checkToken();
+  },
+  methods: {
+    checkToken() {
+      if (this.$store.state.access != "") {
+        this.is_login = true;
+      }
+    },
+  },
 };
 </script>
