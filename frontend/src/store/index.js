545f1157b0aead6cde1b9b479fb08521c5710edb
k-downey <kinchan.yamamoto@gmail.com>
2022-05-05 19:02:31 +0900

    [update]認証機能の追加

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,49 +2,52 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import axios from "axios";
+import Swal from "sweetalert2";
+import router from "../router";
+
 
 Vue.use(Vuex);
-// const base_url = "http://localhost:8000";
+const base_url = "http://localhost:8000";
 
 export default new Vuex.Store({
   state: {
-    token: "",
+    access: "",
+    refresh: "",
   },
-  mutations: {},
+  mutations: {
+    set_token(state, data) {
+      state.access = data["access"];
+      state.refresh = data["refresh"];
+    },
+  },
   actions: {
-    login(context) {
-      console.log(context);
+    login(context, credentials) {
       var FormData = require("form-data");
       var data = new FormData();
-      data.append("email", "hello@hello.co.jp");
-      data.append("password", "testpassword");
+      data.append("email", credentials["email"]);
+      data.append("password", credentials["password"]);
 
-      var config = {
-        method: "post",
-        url: "http://localhost:8000/api/auth/jwt/create/",
-        headers: {
-        },
-        data: data,
-      };
-
-      axios(config)
+      const url = base_url + "/api/auth/jwt/create/";
+      console.log(data);
+      axios
+        .post(url, data)
         .then(function (response) {
-          console.log(JSON.stringify(response.data));
+          context.commit("set_token", response.data);
+          router.push("/graph");
         })
         .catch(function (error) {
-          console.log(error);
+          if (error.response.status == 401) {
+            Swal.fire({
+              icon: "warning",
+              title: "Error",
+              text: "ユーザー名もしくはパスワード、または両方が間違っています",
+              showConfirmButton: false,
+              showCloseButton: false,
+              timer: 3000,
+            });
+          }
         });
-
-      // const url = base_url + "/api/auth/jwt/create/";
-      // axios
-      //   .post(url, credentials)
-      //   .then(function (response) {
-      //     console.log(response);
-      //   })
-      //   .catch(function (error) {
-      //     console.error(error);
-      //   });
-      // console.log(context);
+      console.log(context);
     },
   },
   modules: {},
