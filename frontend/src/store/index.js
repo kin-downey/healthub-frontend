46f3817a62244cc8b360ca32e7b15839a1ecb7ae
k-downey <kinchan.yamamoto@gmail.com>
2022-05-06 18:36:21 +0900

    [update]本登録用メール再送信機能の作成

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import Swal from "sweetalert2";
 import router from "../router";
 
-import createPersistedstate from 'vuex-persistedstate'
+import createPersistedstate from "vuex-persistedstate";
 
 Vue.use(Vuex);
 const base_url = "http://localhost:8000";
@@ -14,46 +14,56 @@ export default new Vuex.Store({
   state: {
     access: "",
     refresh: "",
-    pre_registed_email: "",
+    pre_registed_email: "test@gmail.com",
     is_login: "",
   },
   mutations: {
-    set_access_token(state, data){
+    set_access_token(state, data) {
       state.access = data["access"];
     },
     set_token(state, data) {
       state.access = data["access"];
       state.refresh = data["refresh"];
-      state.is_login = true
+      state.is_login = true;
     },
-    set_pre_email(state, data){
+    set_pre_email(state, data) {
       state.pre_registed_email = data;
     },
-    logout(state){
-      state.access = ""
-      state.refresh = ""
-      state.pre_registed_email = ""
-      state.is_login = false
+    logout(state) {
+      state.access = "";
+      state.refresh = "";
+      state.pre_registed_email = "";
+      state.is_login = false;
     },
   },
   actions: {
-    logout(context){
-      router.push("/")
-      context.commit("logout")
+    resend_email(context, email) {
+      const url = base_url + "/account/auth/users/resend_activation/"
+      var FormData = require("form-data");
+      var data = new FormData();
+      data.append("email", email);
+      axios.post(url, data)
+      .catch(function(error){
+        console.log(error)
+      })
     },
-    refresh_token(context, refresh){
-      if (refresh != ""){
-        const url = base_url + "/account/auth/jwt/refresh/"
+    logout(context) {
+      router.push("/");
+      context.commit("logout");
+    },
+    refresh_token(context, refresh) {
+      if (refresh != "") {
+        const url = base_url + "/account/auth/jwt/refresh/";
         var data = new FormData();
-        data.append("refresh", refresh)
+        data.append("refresh", refresh);
         axios
-        .post(url, data)
-        .then(function(response){
-          context.commit("set_access_token", response.data)
-        })
-        .catch(function(){
-          context.commit("logout")
-        })
+          .post(url, data)
+          .then(function (response) {
+            context.commit("set_access_token", response.data);
+          })
+          .catch(function () {
+            context.commit("logout");
+          });
       }
     },
     login(context, credentials) {
@@ -91,16 +101,18 @@ export default new Vuex.Store({
       data.append("password", credentials["password"]);
       data.append("re_password", credentials["re_password"]);
 
-
       const url = base_url + "/account/auth/users/";
       axios
         .post(url, data)
         .then(function (response) {
-          context.commit("set_pre_email", response.data.email)
-          router.push("/register")
+          context.commit("set_pre_email", response.data.email);
+          router.push("/register");
         })
         .catch(function (error) {
-          if (error.response.data.password[0] == "The password is too similar to the メールアドレス."){
+          if (
+            error.response.data.password[0] ==
+            "The password is too similar to the メールアドレス."
+          ) {
             Swal.fire({
               icon: "warning",
               title: "Error",
@@ -109,8 +121,9 @@ export default new Vuex.Store({
               showCloseButton: false,
               timer: 3000,
             });
-          }
-          else if (error.response.data.email[0] == "Enter a valid email address.") {
+          } else if (
+            error.response.data.email[0] == "Enter a valid email address."
+          ) {
             Swal.fire({
               icon: "warning",
               title: "Error",
@@ -119,8 +132,10 @@ export default new Vuex.Store({
               showCloseButton: false,
               timer: 3000,
             });
-          }
-          else if (error.response.data.email[0] == "user account with this メールアドレス already exists."){
+          } else if (
+            error.response.data.email[0] ==
+            "user account with this メールアドレス already exists."
+          ) {
             Swal.fire({
               icon: "warning",
               title: "Error",
@@ -129,8 +144,7 @@ export default new Vuex.Store({
               showCloseButton: false,
               timer: 3000,
             });
-          }
-          else{
+          } else {
             Swal.fire({
               icon: "warning",
               title: "Error",
@@ -144,5 +158,5 @@ export default new Vuex.Store({
     },
   },
   modules: {},
-  plugins: [createPersistedstate({ key: 'healthub' })],
+  plugins: [createPersistedstate({ key: "healthub" })],
 });
--- frontend/src/views/WaitEmail.vue
+++ frontend/src/views/WaitEmail.vue
@@ -1,5 +1,70 @@
 <template>
-    <div class="wait-email">
-        <p>this is a wait email page</p>
-    </div>
+  <div class="wait-email">
+    <v-card class="mx-auto mt-10" max-width="95%">
+      <v-card-text>
+        <p class="text-h5 text--primary">
+          {{ email }}へ<br />メールを送信しました
+        </p>
+        <div class="text--primary">
+          <br />
+          メールが届きませんか？以下をご確認ください<br />
+          <br />
+          ・表示されているメールアドレスは正しいですか？<br />
+          ・迷惑メールボックスに入っていませんか？<br />
+          <br />
+          もしくは，「メールを再送信」」ボタンを押してください
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-col align="end">
+          <v-btn @click="resend_email()" text color="primary">
+            メールを再送信
+          </v-btn>
+        </v-col>
+      </v-card-actions>
+    </v-card>
+    <v-snackbar v-model="snackbar" top color="success">
+      <v-row align-content="center">
+        <v-col align="center">
+          <p class="text-h6 mt-4">メールを再送信しました</p>
+        </v-col>
+      </v-row>
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="red"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+          height="auto"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      snackbar: false,
+    };
+  },
+  computed: {
+    email() {
+      return this.$store.state.pre_registed_email;
+    },
+  },
+  methods: {
+    resend_email() {
+      const _this = this;
+      this.$store
+        .dispatch("resend_email", this.$store.state.pre_registed_email)
+        .then(function () {
+          _this.snackbar = true;
+        });
+    },
+  },
+};
+</script>
